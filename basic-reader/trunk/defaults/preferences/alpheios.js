fa8d624f9c9b8c3bb920181aba7fcfc4cc8c003f
MichaelGursky <MichaelGursky@6cdfca17-2474-42dc-bd0f-2d6b4f5baa86>
2008-06-04 23:31:49 +0000

    Rename from melampus/mp to alpheios/alph

---
+++ basic-reader/trunk/defaults/preferences/alpheios.js
@@ -0,0 +1,11 @@
+pref("extensions.alpheios.debug", false);
+pref("extensions.alpheios.default_language","");
+pref("extensions.alpheios.mhttpd.port",8200); 
+pref("extensions.alpheios.popuptrigger",'mousemove');
+pref("extensions.alpheios.keys.toggle",'m');
+pref("extensions.alpheios.keymodifiers.toggle",'alt');
+pref("extensions.alpheios.grammar.window.loc",
+        "bottomright");
+pref("extensions.alpheios.shift.window.loc",
+        "topleft");
+ 
