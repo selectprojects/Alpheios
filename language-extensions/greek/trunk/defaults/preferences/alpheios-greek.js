eca8a1b62782098a61e7f6582144badea6086301
BridgetAlmas <BridgetAlmas@6cdfca17-2474-42dc-bd0f-2d6b4f5baa86>
2008-06-05 14:06:19 +0000

    Initial build of the renamed Alpheios-Greek tools. - Same GUID as Melampus Greek Tools but increased release # to 0.2 - Increased minimum required version of the main basic-reader extension to 0.3 - Also, reverted the url for the lexicon in the preferences back to http://perseus.harrysdell.com:8008/melampus/xmlmorph-mp.js as we havne't yet deployed the renamed perseus code.

--- language-extensions/greek/trunk/defaults/preferences/alpheios-greek.js
+++ language-extensions/greek/trunk/defaults/preferences/alpheios-greek.js
@@ -4,7 +4,7 @@ pref("extensions.alpheios.greek.languagecode","grc");
 pref("extensions.alpheios.greek.base_unit","word");
 pref("extensions.alpheios.greek.methods.lexicon",'webservice');
 pref("extensions.alpheios.greek.url.lexicon", 'http://perseus.harrysdell.com:8008');
-pref("extensions.alpheios.greek.url.lexicon.request", "/hopper/alpheios/xmlmorph-mp.jsp?word=<WORD>&input=Unicode");
+pref("extensions.alpheios.greek.url.lexicon.request", "/hopper/melampus/xmlmorph-mp.jsp?word=<WORD>&input=Unicode");
 pref("extensions.alpheios.greek.url.lexicon.timeout",10000);
 pref("extensions.alpheios.greek.popuptrigger",'mousemove');
             
