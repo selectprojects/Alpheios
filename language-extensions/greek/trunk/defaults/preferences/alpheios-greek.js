5199542718c04367f6b6eb3b413d9a4eb82b2591
BridgetAlmas <BridgetAlmas@6cdfca17-2474-42dc-bd0f-2d6b4f5baa86>
2008-06-09 16:21:44 +0000

    Switching to the new Perseus build and the renamed Alpheios code as the default for the greek lookup.

--- language-extensions/greek/trunk/defaults/preferences/alpheios-greek.js
+++ language-extensions/greek/trunk/defaults/preferences/alpheios-greek.js
@@ -3,8 +3,8 @@ pref("extensions.alpheios.greek.chromepkg","alpheios-greek");
 pref("extensions.alpheios.greek.languagecode","grc");
 pref("extensions.alpheios.greek.base_unit","word");
 pref("extensions.alpheios.greek.methods.lexicon",'webservice');
-pref("extensions.alpheios.greek.url.lexicon", 'http://perseus.harrysdell.com:8008');
-pref("extensions.alpheios.greek.url.lexicon.request", "/hopper/melampus/xmlmorph-mp.jsp?word=<WORD>&input=Unicode");
+pref("extensions.alpheios.greek.url.lexicon", 'http://perseus.harrysdell.com:8020');
+pref("extensions.alpheios.greek.url.lexicon.request", "/hopper/alpheios/xmlmorph-alph.jsp?word=<WORD>&input=Unicode");
 pref("extensions.alpheios.greek.url.lexicon.timeout",10000);
 pref("extensions.alpheios.greek.popuptrigger",'mousemove');
             
