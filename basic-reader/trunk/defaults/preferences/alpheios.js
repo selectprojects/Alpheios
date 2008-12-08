c070c59cba1bf7fcc8ebbd7debbe83cb1b59aabd
BridgetAlmas <BridgetAlmas@6cdfca17-2474-42dc-bd0f-2d6b4f5baa86>
2008-12-08 19:50:35 +0000

    making panel changes sticky by default

--- basic-reader/trunk/defaults/preferences/alpheios.js
+++ basic-reader/trunk/defaults/preferences/alpheios.js
@@ -14,7 +14,7 @@ pref("extensions.alpheios.cmds.alpheios-inflect-cmd","handleInflections");
 pref("extensions.alpheios.cmds.alpheios-morph-inflect-cmd","handleInflectionsForMorphWindow");
 pref("extensions.alpheios.cmds.alpheios-grammar-cmd","openGrammar");
 pref("extensions.alpheios.ped-prototype-match","file:.*Perseus.text.1999.01.0133|http:\/\/dev.alpheios.net:8020\/(alpheios_proto)|(hopper\/alpheios\/text-alph.jsp)");
-pref("extensions.alpheios.panels.sticky",false);
+pref("extensions.alpheios.panels.sticky",true);
 pref("extensions.alpheios.panels.alph-morph-panel.pedagogical",1);
 pref("extensions.alpheios.panels.alph-morph-panel.basic",0);
 pref("extensions.alpheios.panels.alph-dict-panel.pedagogical",1);
