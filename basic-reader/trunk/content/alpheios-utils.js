083fb298a4540e956c4468e50c2c2262592234a3
MichaelGursky <MichaelGursky@6cdfca17-2474-42dc-bd0f-2d6b4f5baa86>
2009-04-02 16:33:53 +0000

    Replace jquery-1.2.6.min.js with version that fixes className comparisons for SVG nodes. Update jquery-1.2.1 to jquery-1.2.6. Delete old/replaced versions of jquery library.

--- basic-reader/trunk/content/alpheios-utils.js
+++ basic-reader/trunk/content/alpheios-utils.js
@@ -36,7 +36,7 @@ if (typeof Alph == "undefined") {
     // in which case it might be sufficient to skip the extreme flag
     Components.classes["@mozilla.org/moz/jssubscript-loader;1"]
           .getService(Components.interfaces.mozIJSSubScriptLoader)
-          .loadSubScript("chrome://alpheios/content/jquery-1.2.6.min.js", Alph);
+          .loadSubScript("chrome://alpheios/content/jquery-1.2.6-alph.js", Alph);
     Alph.$ = jQuery.noConflict(true);
     
 }
