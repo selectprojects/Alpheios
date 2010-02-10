c885d4df1d08469bed689ef73b1293cef9882284
BridgetAlmas <BridgetAlmas@6cdfca17-2474-42dc-bd0f-2d6b4f5baa86>
2010-02-10 13:03:37 +0000

    Linux build for bug 387

--- mhttpd/trunk/src/make_linux.sh
+++ mhttpd/trunk/src/make_linux.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 # 32-bit version
-/usr/bin/gcc34 mhttpd.cpp -o ../dist/Linux_x86-gcc3/mhttpd -lstdc++ -m32 -v -v
+rm -f ../dist/Linux_x86-gcc3/mhttpd
+/usr/bin/gcc-3.4 mhttpd.cpp -o ../dist/Linux_x86-gcc3/mhttpd -lstdc++ -m32 -v -v
 strip ../dist/Linux_x86-gcc3/mhttpd
 # 64 bit version
-/usr/bin/gcc34 mhttpd.cpp -o ../dist/Linux_x86_64-gcc3/mhttpd -lstdc++ -v -v
+rm -f ../dist/Linux_x86_64-gcc3/mhttpd
+/usr/bin/gcc-3.4 mhttpd.cpp -o ../dist/Linux_x86_64-gcc3/mhttpd -lstdc++ -v -v
 strip ../dist/Linux_x86_64-gcc3/mhttpd
 
