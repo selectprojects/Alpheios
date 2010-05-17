b23899734d7f6066bd8249722552237e2f5d013e
BridgetAlmas <BridgetAlmas@6cdfca17-2474-42dc-bd0f-2d6b4f5baa86>
2010-05-17 00:44:32 +0000

    switch to remote aramorph service

--- language-extensions/arabic/trunk/defaults/preferences/alpheios-arabic.js
+++ language-extensions/arabic/trunk/defaults/preferences/alpheios-arabic.js
@@ -1,13 +1,13 @@
-pref("extensions.alpheios.arabic.usemhttpd", true);
+pref("extensions.alpheios.arabic.usemhttpd", false);
 pref("extensions.alpheios.arabic.chromepkg","alpheios-arabic");
 pref("extensions.alpheios.arabic.languagecode","ara,ar");
 pref("extensions.alpheios.arabic.base_unit","word");
 pref("extensions.alpheios.arabic.methods.startup",'loadLexIds,loadStripper');
 pref("extensions.alpheios.arabic.methods.convert", 'unicodeToBuckwalter');
 pref("extensions.alpheios.arabic.methods.lexicon",'webservice');
-pref("extensions.alpheios.arabic.url.lexicon", 'http://localhost:8200');
-pref("extensions.alpheios.arabic.url.lexicon.request", "/arabic?word=<WORD>");
-pref("extensions.alpheios.arabic.url.lexicon.timeout",5000);
+pref("extensions.alpheios.arabic.url.lexicon", 'http://alpheios.net/perl/aramorph?');
+pref("extensions.alpheios.arabic.url.lexicon.request", "word=<WORD>");
+pref("extensions.alpheios.arabic.url.lexicon.timeout",10000);
 pref("extensions.alpheios.arabic.grammar.hotlinks","");
 pref("extensions.alpheios.arabic.popuptrigger",'dblclick');
 pref("extensions.alpheios.arabic.features.alpheios-inflect",false);
