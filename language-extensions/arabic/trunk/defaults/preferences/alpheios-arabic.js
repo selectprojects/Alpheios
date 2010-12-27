f97a5d8247b117c144026060fd3e5604a256f14e
bmalmas <bmalmas@6cdfca17-2474-42dc-bd0f-2d6b4f5baa86>
2010-12-27 18:39:52 +0000

    adding lane and strings for dict prefs

--- language-extensions/arabic/trunk/defaults/preferences/alpheios-arabic.js
+++ language-extensions/arabic/trunk/defaults/preferences/alpheios-arabic.js
@@ -13,7 +13,7 @@ pref("extensions.alpheios.arabic.popuptrigger",'dblclick');
 pref("extensions.alpheios.arabic.features.alpheios-inflect",false);
 pref("extensions.alpheios.arabic.features.alpheios-grammar",false);
 pref("extensions.alpheios.arabic.panels.use.defaults",true);
-pref("extensions.alpheios.arabic.dictionaries.full","sal");
+pref("extensions.alpheios.arabic.dictionaries.full","lan,sal");
 pref("extensions.alpheios.arabic.dictionary.full.search.url",
      "http://repos.alpheios.net:8080/exist/rest/db/xq/lexi-get.xq?lx=<LEXICON>&lg=ara&out=html");
 pref("extensions.alpheios.arabic.dictionary.full.search.lemma_param","l");
