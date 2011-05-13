cbe210a0d30d6ccde81823ce66293f7ebda63ee6
bmalmas <bmalmas@6cdfca17-2474-42dc-bd0f-2d6b4f5baa86>
2011-05-13 13:44:15 +0000

    setting browse url for Lane and Salmone to Perseus

--- language-extensions/arabic/trunk/defaults/preferences/alpheios-arabic.js
+++ language-extensions/arabic/trunk/defaults/preferences/alpheios-arabic.js
@@ -16,6 +16,14 @@ pref("extensions.alpheios.arabic.panels.use.defaults",true);
 pref("extensions.alpheios.arabic.dictionaries.full","sal,lan");
 pref("extensions.alpheios.arabic.dictionary.full.search.url",
      "http://repos1.alpheios.net/exist/rest/db/xq/lexi-get.xq?lx=<LEXICON>&lg=ara&out=html");
+pref("extensions.alpheios.arabic.dictionary.full.lan.browse.url",
+	 "http://www.perseus.tufts.edu/hopper/text?doc=Perseus%3atext%3a2002.02.0015");
+pref("extensions.alpheios.arabic.dictionary.full.lan.browse.convert_method", 'unicodeToBuckwalter');
+pref("extensions.alpheios.arabic.dictionary.full.lan.browse.root_param",":root=<ROOT>");
+pref("extensions.alpheios.arabic.dictionary.full.sal.browse.url",
+"http://www.perseus.tufts.edu/hopper/text?doc=Perseus%3atext%3a2002.02.0005");
+pref("extensions.alpheios.arabic.dictionary.full.sal.browse.convert_method", 'unicodeToBuckwalter');
+pref("extensions.alpheios.arabic.dictionary.full.sal.browse.root_param",":root=<ROOT>");
 pref("extensions.alpheios.arabic.dictionary.full.search.lemma_param","l");
 pref("extensions.alpheios.arabic.dictionary.full.search.id_param","n");
 pref("extensions.alpheios.arabic.dictionary.full.search.multiple",true);
