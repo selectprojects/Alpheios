2fc68ec7a75af6d5b846f194cb51c2127d93f1c9
bmalmas <bmalmas@6cdfca17-2474-42dc-bd0f-2d6b4f5baa86>
2012-03-12 13:58:20 +0000

    prevent email of some innocuous error message reported by eXist

--- repository/trunk/smtptrigger/src/net/alpheios/log4j/spi/WarnTriggerEvaluator.java
+++ repository/trunk/smtptrigger/src/net/alpheios/log4j/spi/WarnTriggerEvaluator.java
@@ -8,6 +8,19 @@ public class WarnTriggerEvaluator implements TriggeringEventEvaluator
 
 	public boolean isTriggeringEvent(LoggingEvent event) 
 	{ 
-		return event.getLevel().isGreaterOrEqual(Level.WARN); 
+		String[] thrown = event.getThrowableStrRep();
+		boolean showError = true;
+		for (int i=0; i<thrown.length;i++) {
+			if (thrown[i].matches("EOFException(java.net.SocketException: Broken pipe)") ||
+				// this error is often caused by googlebot dropping the ? before the params
+				// in the request
+			    thrown[i].matches(".*java.lang.IllegalArgumentException.*"))
+			{
+				showError = false;
+				break;
+			}
+		}
+		return 
+			showError && event.getLevel().isGreaterOrEqual(Level.WARN); 
 	}	
 }
